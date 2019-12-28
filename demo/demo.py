99d96d00dabd5fcf54de8662f28e1406ee46bf8f
Maarten de Rijk <maarten.derijk@gmail.com>
2019-12-28 16:58:41 +0100

    fixed relative imports

--- demo/demo.py
+++ demo/demo.py
@@ -1,9 +1,10 @@
 ''' Demo site generator'''
+# Relative imports python
 import sys
 sys.path.append(".") 
 
 from time import sleep
-import sitegenerator
+from sitegenerator import SiteGenerator
 from datetime import datetime
 import json
 
@@ -12,9 +13,10 @@ now = datetime.now()
 datestr = now.strftime(r"%Y%m%d%H%M%S")
 
 # Generate a small markup file to use in the main template. Tags with {% date %} and {% time %} are replaced
-snapshot_page = sitegenerator.SiteGenerator()
+snapshot_page = SiteGenerator()
+snapshot_page.template_folder = "./demo/templates"
 snapshot_page.base_template = "base_snapshot.html"
-snapshot_page.output_file = "snapshot_" + datestr + ".html"
+snapshot_page.output_file = ".demo/templates/snapshot_" + datestr + ".html"
 snapshot_page.replacements = {
     "date": now.strftime(r"%d-%m-%Y"),
     "time": now.strftime(r"%H:%M:%S"),
@@ -23,5 +25,5 @@ snapshot_page.render()
 
 
 # Update main template with new templates
-indexpage = sitegenerator.SiteGenerator(output_file="./new_index.html")
+indexpage = SiteGenerator(output_file="./demo/new_index.html")
 indexpage.render()
